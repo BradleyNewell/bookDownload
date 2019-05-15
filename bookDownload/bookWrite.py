13094dc654ae12149bb001a9c131cc7836481433
BradleyXVIII <40966735+BradleyXVIII@users.noreply.github.com>
2019-05-16 00:49:55 +0100

    Update bookWrite.py

--- bookDownload/bookWrite.py
+++ bookDownload/bookWrite.py
@@ -6,8 +6,8 @@ import io
 # To download a different book you can change the string below to your desired book
 html = urlopen("https://www.gutenberg.org/files/2600/2600-0.txt")
 bs = BeautifulSoup(html.read(), "html.parser")
-text = bs.get_text
-text = str(text)
+text = str(bs.get_text)
+
 # Changing the name of "WarandPeace.txt" will change the name of the file you wish to save the book as.
 with io.open("WarandPeace.txt", mode="w", encoding="utf-8") as file:
     file.write(text)
